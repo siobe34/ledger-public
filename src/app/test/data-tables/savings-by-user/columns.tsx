976ae869514500b17611f08ec7017d51424b8447
Ibad Rashid <siobe14@hotmail.com>
2024-03-25 19:32:23 +0000

    data table to show each uesr's expenses and savings

---
+++ src/app/test/data-tables/savings-by-user/columns.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { selectTransactionsSchema } from "@/server/db/schema";
+import { ColumnDef } from "@tanstack/react-table";
+import { z } from "zod";
+
+const _totalBalanceSchema = selectTransactionsSchema
+  .pick({
+    user: true,
+  })
+  .extend({ income: z.number(), expenses: z.number(), savings: z.number() });
+
+export type UserSavings = z.infer<typeof _totalBalanceSchema>;
+
+export const columns: ColumnDef<UserSavings>[] = [
+  {
+    accessorKey: "user",
+    header: "User",
+  },
+  {
+    accessorKey: "income",
+    header: "Income",
+    // TODO: extract monetary value sanitization to function because it's used in a lot of places. Also use zod for validation in the function
+    cell: ({ row }) => {
+      let unsafeAmount = parseFloat(row.getValue("income"));
+      if (isNaN(unsafeAmount)) {
+        unsafeAmount = 0;
+      }
+
+      return `$${unsafeAmount.toFixed(2)}`;
+    },
+  },
+  {
+    accessorKey: "expenses",
+    header: "Expenses",
+    cell: ({ row }) => {
+      let unsafeAmount = parseFloat(row.getValue("expenses"));
+      if (isNaN(unsafeAmount)) {
+        unsafeAmount = 0;
+      }
+
+      return `$${unsafeAmount.toFixed(2)}`;
+    },
+  },
+  {
+    accessorKey: "savings",
+    header: "Savings",
+    cell: ({ row }) => {
+      let unsafeIncome = parseFloat(row.getValue("income"));
+      let unsafeExpenses = parseFloat(row.getValue("expenses"));
+      if (isNaN(unsafeIncome)) {
+        unsafeIncome = 0;
+      }
+      if (isNaN(unsafeExpenses)) {
+        unsafeExpenses = 0;
+      }
+
+      const savings = unsafeIncome - unsafeExpenses;
+      return `$${savings.toFixed(2)}`;
+    },
+  },
+];
---
+++ src/app/test/data-tables/savings-by-user/table.tsx
@@ -0,0 +1,50 @@
+import {
+  UserSavings,
+  columns,
+} from "@/app/test/data-tables/savings-by-user/columns";
+import { DataTable } from "@/components/ui/data-table";
+import { type RequestTransactionData } from "@/server/api/routers/transaction";
+import { api } from "@/trpc/server";
+
+export const SavingsByUserTable = async ({
+  year,
+  month,
+  account,
+  user,
+}: RequestTransactionData) => {
+  const sumPerCategoryWithIncome =
+    await api.transactions.getMonthlySummary.query({
+      year,
+      month,
+      account,
+      user,
+    });
+
+  const uniqueUsers = Array.from(
+    new Set(sumPerCategoryWithIncome.map((i) => i.user)),
+  );
+
+  const savingsByUser: UserSavings[] = [];
+
+  for (const uniqueUser of uniqueUsers) {
+    const income = sumPerCategoryWithIncome
+      .filter((i) => i.user === uniqueUser && i.category === "Income")
+      .map((i) => (i.amount_spent ? +i.amount_spent * -1 : 0))
+      .reduce((acc, curr) => acc + curr, 0);
+
+    const expenses = sumPerCategoryWithIncome
+      .filter((i) => i.user === uniqueUser && i.category !== "Income")
+      .map((i) => (i.amount_spent ? +i.amount_spent : 0))
+      .reduce((acc, curr) => acc + curr, 0);
+
+    const savings = income - expenses;
+
+    savingsByUser.push({ user: uniqueUser, income, expenses, savings });
+  }
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={savingsByUser} />
+    </div>
+  );
+};
