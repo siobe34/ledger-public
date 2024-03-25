22a04663816ccec15794d5945bf14e8116e26971
Ibad Rashid <siobe14@hotmail.com>
2024-03-25 19:32:06 +0000

    data table to show each user's balance by account

---
+++ src/app/test/data-tables/balances-acc-by-user/columns.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { selectTransactionsSchema } from "@/server/db/schema";
+import { ColumnDef } from "@tanstack/react-table";
+import { z } from "zod";
+
+const _acctBalanceSchema = selectTransactionsSchema.pick({
+  account: true,
+  user: true,
+  id: true,
+  balance: true,
+});
+
+export type AccountBalance = z.infer<typeof _acctBalanceSchema>;
+
+export const columns: ColumnDef<AccountBalance>[] = [
+  {
+    accessorKey: "user",
+    header: "User",
+  },
+  {
+    accessorKey: "account",
+    header: "Account",
+  },
+  {
+    accessorKey: "balance",
+    header: "Balance",
+  },
+];
---
+++ src/app/test/data-tables/balances-acc-by-user/table.tsx
@@ -0,0 +1,31 @@
+import { columns } from "@/app/test/data-tables/balances-acc-by-user/columns";
+import { DataTable } from "@/components/ui/data-table";
+import { type RequestTransactionData } from "@/server/api/routers/transaction";
+import { api } from "@/trpc/server";
+
+export const BalancesByAcctUserTable = async ({
+  year,
+  month,
+  account,
+  user,
+}: RequestTransactionData) => {
+  const keyTransactions = await api.transactions.getBalanceByMonth.query({
+    account,
+    user,
+    month,
+    year,
+  });
+
+  const accountBalancesByUser = keyTransactions.map((i) => ({
+    id: i.id,
+    user: i.user,
+    account: i.account,
+    balance: i.balance,
+  }));
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={accountBalancesByUser} />
+    </div>
+  );
+};
