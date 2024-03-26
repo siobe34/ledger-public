f11d627eebe5856b82ac2bd3c33eaa11c7d1b9bf
Ibad Rashid <siobe14@hotmail.com>
2024-03-26 18:40:50 +0000

    empty landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,10 @@
-import { api } from "@/trpc/server";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <a
-        href="/upload"
-        className="rounded border border-blue-500 bg-blue-500 px-4 py-2 text-white hover:bg-blue-800 hover:text-slate-300"
-      >
-        Upload
-      </a>
-      <Test />
-    </main>
+    <a
+      href="/upload"
+      className="rounded border border-blue-500 bg-blue-500 px-4 py-2 text-white hover:bg-blue-800 hover:text-slate-300"
+    >
+      Upload
+    </a>
   );
 }
-
-const Test = async () => {
-  const categoricalSpending = await api.transactions.getMonthlySummary.query({
-    account: "Debit",
-    user: "Ibad",
-    year: 2024,
-    month: 1,
-  });
-  const monthlyBalance = await api.transactions.getBalanceByMonth
-    .query({
-      account: "Debit",
-      user: "someone who doesn't exist",
-      year: 2024,
-      month: 1,
-    })
-    .catch((e) => console.error(e));
-
-  return (
-    <>
-      <p>{JSON.stringify(categoricalSpending)}</p>
-      <p>{JSON.stringify(monthlyBalance)}</p>
-    </>
-  );
-};
