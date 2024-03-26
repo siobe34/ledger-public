a006ef8b12820a9575333b62db73b7adb37b5b08
Ibad Rashid <siobe14@hotmail.com>
2024-03-26 20:13:00 +0000

    placeholder page

--- src/app/dashboard/account-management/page.tsx
+++ src/app/dashboard/account-management/page.tsx
@@ -1,10 +1,3 @@
 export default function AccountManagementPage() {
-  return (
-    <a
-      href="/"
-      className="rounded border border-blue-500 bg-blue-500 px-4 py-2 text-white hover:bg-blue-800 hover:text-slate-300"
-    >
-      Home
-    </a>
-  );
+  return <h1>Account Management</h1>;
 }
