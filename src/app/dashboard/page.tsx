b63d7d80ca9d0c62e28691f4dac62b324fe14759
Ibad Rashid <siobe14@hotmail.com>
2024-03-26 19:45:02 +0000

    dashboard home page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <a
+      href="/"
+      className="rounded border border-blue-500 bg-blue-500 px-4 py-2 text-white hover:bg-blue-800 hover:text-slate-300"
+    >
+      Home
+    </a>
+  );
+}
