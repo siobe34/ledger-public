559dfd651ae49677799f9ed3c5ec16718555c58f
Ibad Rashid <siobe14@hotmail.com>
2024-03-26 20:36:46 +0000

    changed buttons to Links

--- src/app/dashboard/summary/layout.tsx
+++ src/app/dashboard/summary/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function SummaryLayout({
   children,
 }: {
@@ -11,24 +13,24 @@ export default function SummaryLayout({
         tabIndex={-1}
         className="grid h-9 w-full grid-cols-2 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground"
       >
-        <button
-          type="button"
-          role="tab"
-          aria-selected="true"
-          className="inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow"
+        <Link
+          href="/dashboard/summary/monthly"
           tabIndex={0}
-        >
-          Monthly
-        </button>
-        <button
-          type="button"
-          role="tab"
           aria-selected="true"
+          role="tab"
           className="inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow"
-          tabIndex={0}
         >
-          Annual
-        </button>
+          Monthly Summary
+        </Link>
+        <Link
+          href="/dashboard/summary/annual"
+          tabIndex={0}
+          aria-selected="true"
+          role="tab"
+          className="inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow"
+        >
+          Annual Summary
+        </Link>
       </div>
       {children}
     </div>
