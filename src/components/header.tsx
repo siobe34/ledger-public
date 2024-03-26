e77f0fa5274517d427c919863d62048532cf211d
Ibad Rashid <siobe14@hotmail.com>
2024-03-26 19:44:15 +0000

    site header

---
+++ src/components/header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <header className="flex items-center justify-between gap-4 px-4">
+      <div>Ledger</div>
+      <button>Theme Toggle</button>
+    </header>
+  );
+};
