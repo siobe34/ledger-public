e6b3d7a2745a1bf837460838ef85ae2faa30ad57
Ibad Rashid <siobe14@hotmail.com>
2024-03-27 20:52:33 +0000

    layout navigation tabs

---
+++ src/components/nested-layout-nav/layout-nav-tabs.tsx
@@ -0,0 +1,12 @@
+export const NavigationTabs = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <nav
+      role="tablist"
+      aria-orientation="horizontal"
+      tabIndex={-1}
+      className="flex w-full flex-col justify-center gap-4 border-b border-b-primary pb-4 text-muted-foreground sm:flex-row sm:items-center sm:justify-start sm:pb-0"
+    >
+      {children}
+    </nav>
+  );
+};
---
+++ src/components/nested-layout-nav/layout-tab-item.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { cn } from "@/lib/utilsCn";
+import Link, { type LinkProps } from "next/link";
+import { usePathname } from "next/navigation";
+
+type TabItemType = {
+  children: React.ReactNode;
+  className?: string;
+} & LinkProps;
+
+export const TabItem = ({ children, className, ...props }: TabItemType) => {
+  const pathname = usePathname();
+
+  const isLinkActive = pathname === props.href;
+
+  return (
+    <Link
+      {...props}
+      className={cn(
+        "inline-flex items-center justify-center whitespace-nowrap rounded-md border border-primary px-3 py-1 text-sm font-medium ring-offset-background transition-all sm:rounded-b-none sm:border-b-0",
+        "hover:text-foreground",
+        "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
+        "disabled:pointer-events-none disabled:opacity-50",
+        isLinkActive &&
+          "bg-primary text-primary-foreground hover:text-primary-foreground",
+        className,
+      )}
+      role="tab"
+    >
+      {children}
+    </Link>
+  );
+};
