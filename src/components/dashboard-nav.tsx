33df4ad508d57c88ff409ff76e568076365c5824
Ibad Rashid <siobe14@hotmail.com>
2024-03-27 18:13:49 +0000

    animate mobile menu

--- src/components/dashboard-nav.tsx
+++ src/components/dashboard-nav.tsx
@@ -12,35 +12,43 @@ export const DashboardNavigation = () => {
   return (
     <nav className="flex flex-col flex-wrap gap-2 border border-b-0 border-t-0 py-4 sm:p-4">
       <MobileMenuButton className="peer/menu h-12 w-12 self-center sm:hidden" />
-      <ul className="hidden flex-col items-center gap-0 bg-slate-200 peer-[.show-menu]/menu:flex sm:flex sm:gap-4">
-        <li className="w-full">
-          <NavItem href="/" name="Home" icon={<HomeIcon />} />
-        </li>
-        <li className="w-full">
-          <NavItem
-            href="/dashboard/account-management"
-            name="Account Management"
-            icon={<UserRound />}
-          />
-        </li>
-        <li className="w-full">
-          <NavItem
-            href="/dashboard/transactions"
-            name="Transactions"
-            icon={<TableIcon />}
-          />
-        </li>
-        <li className="w-full">
-          <NavItem
-            href="/dashboard/summary/monthly"
-            name="Charts & Analysis"
-            icon={<AreaChart />}
-          />
-        </li>
-        <li className="w-full">
-          <NavItem href="#" name="Log Out" icon={<LogOut />} />
-        </li>
-      </ul>
+      <div className="grid grid-rows-[0fr] transition-all peer-[.show-menu]/menu:grid-rows-[1fr] sm:grid-rows-[1fr]">
+        <div className="overflow-hidden">
+          <ul className="flex flex-col items-center gap-0 sm:gap-4">
+            <li className="w-full">
+              <NavItem
+                href="/dashboard"
+                name="Dashboard Home"
+                icon={<HomeIcon />}
+              />
+            </li>
+            <li className="w-full">
+              <NavItem
+                href="/dashboard/account-management"
+                name="Account Management"
+                icon={<UserRound />}
+              />
+            </li>
+            <li className="w-full">
+              <NavItem
+                href="/dashboard/transactions"
+                name="Transactions"
+                icon={<TableIcon />}
+              />
+            </li>
+            <li className="w-full">
+              <NavItem
+                href="/dashboard/summary/monthly"
+                name="Charts & Analysis"
+                icon={<AreaChart />}
+              />
+            </li>
+            <li className="w-full">
+              <NavItem href="#" name="Log Out" icon={<LogOut />} />
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
