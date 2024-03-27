515c3d1eea077e8160fdda541635424ecacc06cd
Ibad Rashid <siobe14@hotmail.com>
2024-03-27 20:51:57 +0000

    styling for dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({
   return (
     <div className="grid h-full w-full grid-cols-[minmax(0,1fr)] grid-rows-[auto_minmax(0,1fr)] sm:grid-cols-[auto_minmax(0,1fr)] sm:grid-rows-1">
       <DashboardNavigation />
-      <div className="flex w-full flex-col items-center justify-start gap-4 p-8 sm:items-start">
+      <div className="flex w-full flex-col items-center justify-start sm:items-start">
         {children}
       </div>
     </div>
