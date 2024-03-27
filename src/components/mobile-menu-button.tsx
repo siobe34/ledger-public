43c9ab2e1310a7435481ef1512f2e85d8bf7bc22
Ibad Rashid <siobe14@hotmail.com>
2024-03-27 18:13:33 +0000

    hide mobile menu every time url path changes

--- src/components/mobile-menu-button.tsx
+++ src/components/mobile-menu-button.tsx
@@ -2,12 +2,14 @@
 
 import { Button } from "@/components/ui/button";
 import { MenuIcon } from "lucide-react";
-import { ButtonHTMLAttributes, FC, createRef } from "react";
+import { usePathname } from "next/navigation";
+import { ButtonHTMLAttributes, FC, createRef, useEffect } from "react";
 
 export const MobileMenuButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
   children,
   ...props
 }) => {
+  const pathname = usePathname();
   const ref = createRef<HTMLButtonElement>();
 
   const handleClick = () => {
@@ -16,6 +18,14 @@ export const MobileMenuButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
 
     currentRef.classList.toggle("show-menu");
   };
+
+  useEffect(() => {
+    const currentRef = ref.current;
+    if (!currentRef) return;
+
+    currentRef.classList.remove("show-menu");
+  }, [pathname]);
+
   return (
     <Button ref={ref} {...props} onClick={handleClick}>
       <MenuIcon />
