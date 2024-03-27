01632e9ae3b84377e379cc5f13831fae12914334
Ibad Rashid <siobe14@hotmail.com>
2024-03-27 03:12:06 +0000

    added theme toggler

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,10 @@
+import { ToggleTheme } from "@/components/toggle-theme";
+
 export const Header = () => {
   return (
     <header className="flex items-center justify-between gap-4 px-4">
       <div>Ledger</div>
-      <button>Theme Toggle</button>
+      <ToggleTheme />
     </header>
   );
 };
