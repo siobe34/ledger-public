58f7410f12f421011a7dbd4ec2bd158354989956
Ibad Rashid <siobe14@hotmail.com>
2024-03-27 15:32:24 +0000

    show bottom border in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { ToggleTheme } from "@/components/toggle-theme";
 
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between gap-4 px-4">
+    <header className="flex items-center justify-between gap-4 border-b px-4">
       <div>Ledger</div>
       <ToggleTheme />
     </header>
