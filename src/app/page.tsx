396d74349900c532a0aa0f0f9ddec942857ef94a
Ibad Rashid <siobe14@hotmail.com>
2024-03-27 01:48:17 +0000

    dummy link to Dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
     <a
-      href="/upload"
+      href="/dashboard"
       className="rounded border border-blue-500 bg-blue-500 px-4 py-2 text-white hover:bg-blue-800 hover:text-slate-300"
     >
-      Upload
+      Dashboard
     </a>
   );
 }
