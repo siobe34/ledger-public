b928c0710568d3422062355468fc813b8b7865ce
Ibad Rashid <siobe14@hotmail.com>
2024-03-27 15:32:45 +0000

    testing theme colours

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 export default function Home() {
   return (
-    <a
-      href="/dashboard"
-      className="rounded border border-blue-500 bg-blue-500 px-4 py-2 text-white hover:bg-blue-800 hover:text-slate-300"
-    >
-      Dashboard
-    </a>
+    <section>
+      <button className="rounded bg-secondary p-4 text-secondary-foreground">
+        Test
+      </button>
+      <a
+        href="/dashboard"
+        className="rounded border border-blue-500 bg-blue-500 px-4 py-2 text-white hover:bg-blue-800 hover:text-slate-300"
+      >
+        Dashboard
+      </a>
+    </section>
   );
 }
