93eeba0855c0edeed2cd6a7842dae0c8938a37de
Ibad Rashid <siobe14@hotmail.com>
2024-03-27 17:36:31 +0000

    fleshed out dashboard navigation

--- src/components/dashboard-nav.tsx
+++ src/components/dashboard-nav.tsx
@@ -1,48 +1,44 @@
-import Link, { type LinkProps } from "next/link";
+import { MobileMenuButton } from "@/components/mobile-menu-button";
+import { NavItem } from "@/components/nav-item";
+import {
+  AreaChart,
+  HomeIcon,
+  LogOut,
+  TableIcon,
+  UserRound,
+} from "lucide-react";
 
-type NavItemType = {
-  name: string;
-  icon: string;
-} & LinkProps;
-
-const NavItem = ({ name, icon, ...props }: NavItemType) => {
-  return (
-    <Link
-      {...props}
-      className="relative inline-flex cursor-pointer items-center justify-center rounded p-2 after:absolute after:top-full after:mt-2 after:hidden after:w-[130%] sm:relative sm:after:block"
-    >
-      {icon}
-      <span className="z-10 ml-2 sm:absolute sm:left-full sm:hidden sm:whitespace-nowrap sm:rounded sm:px-1 sm:text-sm">
-        {name}
-      </span>
-    </Link>
-  );
-};
 export const DashboardNavigation = () => {
   return (
-    <nav className="flex flex-col flex-wrap p-8">
-      <button className="flex self-center sm:hidden">Mobile Menu Button</button>
-      <ul className="hidden flex-col gap-4 sm:flex">
-        <li>
+    <nav className="flex flex-col flex-wrap gap-2 border border-b-0 border-t-0 py-4 sm:p-4">
+      <MobileMenuButton className="peer/menu h-12 w-12 self-center sm:hidden" />
+      <ul className="hidden flex-col items-center gap-0 bg-slate-200 peer-[.show-menu]/menu:flex sm:flex sm:gap-4">
+        <li className="w-full">
+          <NavItem href="/" name="Home" icon={<HomeIcon />} />
+        </li>
+        <li className="w-full">
           <NavItem
             href="/dashboard/account-management"
             name="Account Management"
-            icon="X"
+            icon={<UserRound />}
           />
         </li>
-        <li>
+        <li className="w-full">
           <NavItem
             href="/dashboard/transactions"
             name="Transactions"
-            icon="Y"
+            icon={<TableIcon />}
           />
         </li>
-        <li>
+        <li className="w-full">
           <NavItem
             href="/dashboard/summary/monthly"
             name="Charts & Analysis"
-            icon="Z"
+            icon={<AreaChart />}
           />
+        </li>
+        <li className="w-full">
+          <NavItem href="#" name="Log Out" icon={<LogOut />} />
         </li>
       </ul>
     </nav>
