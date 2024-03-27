195b31bed9693d5f2de2936b16a33863ba4cf932
Ibad Rashid <siobe14@hotmail.com>
2024-03-27 18:20:19 +0000

    vertical padding

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { ToggleTheme } from "@/components/toggle-theme";
 
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between gap-4 border-b px-4">
+    <header className="flex items-center justify-between gap-4 border-b px-4 py-2">
       <div>Ledger</div>
       <ToggleTheme />
     </header>
