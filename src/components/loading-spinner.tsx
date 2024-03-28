1518188c18dc5b5560a6265239dea9e31a06f2b8
Ibad Rashid <siobe14@hotmail.com>
2024-03-28 17:39:54 +0000

    loading spinner component

---
+++ src/components/loading-spinner.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utilsCn";
+import { FC, HTMLAttributes } from "react";
+
+export const LoadingSpinner: FC<HTMLAttributes<HTMLDivElement>> = ({
+  className,
+  ...props
+}) => {
+  return (
+    <div
+      {...props}
+      className={cn(
+        "flex h-24 w-24 items-center justify-center self-center",
+        className,
+      )}
+    >
+      <div className="h-full w-full animate-spin rounded-full border-8 border-t-primary" />
+    </div>
+  );
+};
