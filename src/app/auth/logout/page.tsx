10b01f6efb90c2275442ca3ee90a8f301b260f1c
Ibad Rashid <siobe14@hotmail.com>
2024-03-28 22:47:33 +0000

    logout page

---
+++ src/app/auth/logout/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { createClient } from "@/lib/supabase/client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function LogoutPage() {
+  const router = useRouter();
+  const supabase = createClient();
+
+  useEffect(() => {
+    const signOut = async () => {
+      const status = await supabase.auth.signOut();
+
+      if (status.error) {
+        router.push("/error");
+      }
+      router.replace("/auth/login");
+    };
+    signOut();
+  }, []);
+
+  return (
+    <Card className="mx-2 mt-12">
+      <CardHeader>
+        <CardTitle>Ledger</CardTitle>
+        <CardDescription>All your transaction in one place.</CardDescription>
+      </CardHeader>
+
+      <CardContent className="text-lg">Signing out...</CardContent>
+    </Card>
+  );
+}
