af4780dd31695cf98caf827ffc942b32cf849108
Ibad Rashid <siobe14@hotmail.com>
2024-03-29 23:34:34 +0000

    changed transactions table to client component to show data status notifications

--- src/app/dashboard/transactions/transactions-data-table/table.tsx
+++ src/app/dashboard/transactions/transactions-data-table/table.tsx
@@ -1,25 +1,58 @@
+"use client";
+
 import { columns } from "@/app/dashboard/transactions/transactions-data-table/columns";
 import { DataTable } from "@/components/ui/data-table";
+import { MONTHS } from "@/lib/constants/months";
+// TODO: change location of import to not be from server
 import { type RequestTransactionData } from "@/server/api/routers/transaction";
-import { api } from "@/trpc/server";
+import { api } from "@/trpc/react";
+import { RefreshCw } from "lucide-react";
+import { toast } from "sonner";
 
-export const QueriedTransactionsTable = async ({
+export const QueriedTransactionsTable = ({
   year,
   month,
   account,
   user,
 }: RequestTransactionData) => {
-  const queriedTransactions = await api.transactions.getByMonth.query({
-    year,
-    month,
-    account,
-    user,
-  });
+  const { data, isError, isLoading, isSuccess } =
+    api.transactions.getByMonth.useQuery({
+      year,
+      month,
+      account,
+      user,
+    });
+
+  if (isError) {
+    toast.error(
+      "Unable to load Transactions data at this time. Please try again.",
+    );
+    return null;
+  }
+
+  if (isLoading) {
+    toast(`Loading transactions for ${MONTHS[month - 1]!.label}, ${year}.`, {
+      icon: <RefreshCw className="h-4 animate-spin" />,
+    });
+  }
+
+  if (isSuccess && data.length === 0) {
+    toast.info(
+      `No Transactions exist for ${MONTHS[month - 1]!.label}, ${year}. Head over to the Upload page to load them in.`,
+    );
+  }
+  if (isSuccess && data.length > 0) {
+    toast.success(
+      `Transactions successfully loaded for ${MONTHS[month - 1]!.label}, ${year}.`,
+    );
+  }
+
+  if (!data) return null;
 
   return (
     // TODO: needs responsive container
     <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={queriedTransactions} />
+      <DataTable columns={columns} data={data} />
     </div>
   );
 };
