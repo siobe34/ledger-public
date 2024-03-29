4f72565b0f613580439b405e3fc7d25230db45be
Ibad Rashid <siobe14@hotmail.com>
2024-03-29 01:00:52 +0000

    adding logo to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,10 @@
+import { Logo } from "@/components/logo";
 import { ToggleTheme } from "@/components/toggle-theme";
 
 export const Header = () => {
   return (
     <header className="flex items-center justify-between gap-4 border-b px-4 py-2">
-      <div>Ledger</div>
+      <Logo />
       <ToggleTheme />
     </header>
   );
