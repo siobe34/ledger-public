cfb1d4242f755f8265213849f708f9538387be54
Ibad Rashid <siobe14@hotmail.com>
2024-03-30 20:31:34 +0000

    moved toast declarations inside useEffect and added Loading state

--- src/app/dashboard/transactions/transactions-data-table/table.tsx
+++ src/app/dashboard/transactions/transactions-data-table/table.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { columns } from "@/app/dashboard/transactions/transactions-data-table/columns";
+import { LoadingSpinner } from "@/components/loading-spinner";
 import { DataTable } from "@/components/ui/data-table";
 import { MONTHS } from "@/lib/constants/months";
 // TODO: change location of import to not be from server
 import { type RequestTransactionData } from "@/server/api/routers/transaction";
 import { api } from "@/trpc/react";
 import { RefreshCw } from "lucide-react";
+import { useEffect } from "react";
 import { toast } from "sonner";
 
 export const QueriedTransactionsTable = ({
@@ -23,31 +25,32 @@ export const QueriedTransactionsTable = ({
       user,
     });
 
-  if (isError) {
-    toast.error(
-      "Unable to load Transactions data at this time. Please try again.",
-    );
-    return null;
-  }
+  useEffect(() => {
+    if (isError) {
+      toast.error(
+        "Unable to load Transactions data at this time. Please try again.",
+      );
+    }
 
-  if (isLoading) {
-    toast(`Loading transactions for ${MONTHS[month - 1]!.label}, ${year}.`, {
-      icon: <RefreshCw className="h-4 animate-spin" />,
-    });
-  }
+    if (isLoading) {
+      toast(`Loading transactions for ${MONTHS[month - 1]!.label}, ${year}.`, {
+        icon: <RefreshCw className="h-4 animate-spin" />,
+      });
+    }
 
-  if (isSuccess && data.length === 0) {
-    toast.info(
-      `No Transactions exist for ${MONTHS[month - 1]!.label}, ${year}. Head over to the Upload page to load them in.`,
-    );
-  }
-  if (isSuccess && data.length > 0) {
-    toast.success(
-      `Transactions successfully loaded for ${MONTHS[month - 1]!.label}, ${year}.`,
-    );
-  }
+    if (isSuccess && data.length === 0) {
+      toast.info(
+        `No Transactions exist for ${MONTHS[month - 1]!.label}, ${year}. Head over to the Upload page to load them in.`,
+      );
+    }
+    if (isSuccess && data.length > 0) {
+      toast.success(
+        `Transactions successfully loaded for ${MONTHS[month - 1]!.label}, ${year}.`,
+      );
+    }
+  }, [isError, isLoading, isSuccess, data, month, year]);
 
-  if (!data) return null;
+  if (!data) return <LoadingSpinner />;
 
   return (
     // TODO: needs responsive container
