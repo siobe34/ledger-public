c4edcfb0641b6218ff87dff105a1318870040387
Ibad Rashid <siobe14@hotmail.com>
2024-03-31 12:06:34 +0000

    highlight dashboard navigation links when they're active

--- src/components/dashboard-nav/nav-item.tsx
+++ src/components/dashboard-nav/nav-item.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { cn } from "@/lib/utilsCn";
 import { type LucideProps } from "lucide-react";
 import Link, { type LinkProps } from "next/link";
+import { usePathname } from "next/navigation";
 
 type NavItemType = {
   name: string;
@@ -13,10 +17,22 @@ type NavItemType = {
 } & LinkProps;
 
 export const NavItem = ({ name, icon, ...props }: NavItemType) => {
+  let pathnameArray = usePathname().split("/");
+  let linkPathArray = (props.href as string).split("/");
+
+  if (pathnameArray.length > 3) pathnameArray = pathnameArray.slice(0, 3);
+  if (linkPathArray.length > 3) linkPathArray = linkPathArray.slice(0, 3);
+
+  const isLinkActive = pathnameArray.toString() === linkPathArray.toString();
+
   return (
     <Link
       {...props}
-      className="inline-flex w-full items-center justify-center gap-2 rounded py-4 text-foreground/70 hover:text-primary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 sm:aspect-square sm:h-8"
+      className={cn(
+        "inline-flex w-full items-center justify-center gap-2 rounded py-4 text-foreground/70 hover:text-primary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 sm:aspect-square sm:h-8",
+        isLinkActive &&
+          "border border-primary bg-primary text-primary-foreground hover:text-primary-foreground",
+      )}
     >
       <TooltipProvider delayDuration={75}>
         <Tooltip>
