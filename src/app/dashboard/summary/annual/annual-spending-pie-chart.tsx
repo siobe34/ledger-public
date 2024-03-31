770dda0c83102c6707df2d83948f1616cdcb34e2
Ibad Rashid <siobe14@hotmail.com>
2024-03-31 14:18:59 +0000

    pie chart to show annual spending by category

---
+++ src/app/dashboard/summary/annual/annual-spending-pie-chart.tsx
@@ -0,0 +1,53 @@
+import { PieChart } from "@/components/pie-chart";
+import { type RequestTransactionData } from "@/server/api/routers/transaction";
+import { api } from "@/trpc/server";
+
+export const AnnualSpendingPieChart = async ({
+  year,
+}: Pick<RequestTransactionData, "year">) => {
+  const categoricalDataWithIncome =
+    await api.transactions.getAnnualCategoricalSpending.query({
+      year,
+    });
+  const categoricalSpending = categoricalDataWithIncome.filter(
+    (i) => i.category !== "Income",
+  );
+
+  const labels = Array.from(
+    new Set(categoricalSpending.map((i) => i.category)),
+  );
+
+  const amountByCategory = labels.map((category) =>
+    categoricalSpending
+      .filter((i) => i.category === category)
+      .map((i) => (i.amount_spent ? +i.amount_spent : 0))
+      .reduce((pv, cv) => pv + cv),
+  );
+
+  return (
+    <PieChart
+      data={{
+        labels,
+        datasets: [
+          {
+            label: "Amount Spent",
+            data: amountByCategory,
+            borderWidth: 0,
+            hoverBorderWidth: 1,
+            hoverOffset: 10,
+          },
+        ],
+      }}
+      options={{
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          title: {
+            display: false,
+            text: "Annual Categorical Spending",
+          },
+        },
+      }}
+    />
+  );
+};
