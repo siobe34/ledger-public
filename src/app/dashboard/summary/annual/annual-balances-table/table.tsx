db178080a3182c874937a87ff4a6fb989080f45b
Ibad Rashid <siobe14@hotmail.com>
2024-03-31 03:48:52 +0000

    display notifications when loading annual data

--- src/app/dashboard/summary/annual/annual-balances-table/table.tsx
+++ src/app/dashboard/summary/annual/annual-balances-table/table.tsx
@@ -1,18 +1,34 @@
-import { columns } from "@/app/dashboard/summary/annual/annual-balances-table/columns";
-import { DataTable } from "@/components/ui/data-table";
-import { type RequestTransactionData } from "@/server/api/routers/transaction";
-import { api } from "@/trpc/server";
+"use client";
 
-export const AnnualBalancesTable = async ({
+import { columns } from "@/app/dashboard/summary/annual/annual-balances-table/columns";
+import { LoadingSpinner } from "@/components/loading-spinner";
+import { DataTable } from "@/components/ui/data-table";
+import { dataLoadToastNotifications } from "@/lib/dataLoadToastNotifications";
+import { type RequestTransactionData } from "@/server/api/routers/transaction";
+import { api } from "@/trpc/react";
+import { useEffect } from "react";
+
+export const AnnualBalancesTable = ({
   year,
 }: Pick<RequestTransactionData, "year">) => {
-  const annualBalancesByUser = await api.transactions.getBalancesByYear.query({
-    year,
-  });
+  const { data, isError, isLoading, isSuccess } =
+    api.transactions.getBalancesByYear.useQuery({
+      year,
+    });
 
-  return (
-    <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={annualBalancesByUser} />
-    </div>
-  );
+  useEffect(() => {
+    dataLoadToastNotifications({
+      isError,
+      isLoading,
+      isSuccess,
+      year,
+      dataLength: data?.length ?? 0,
+    });
+  }, [isError, isLoading, isSuccess, data, year]);
+
+  if (!data) return <LoadingSpinner className="mx-auto" />;
+
+  const annualBalancesByUser = data;
+
+  return <DataTable columns={columns} data={annualBalancesByUser} />;
 };
