c75c8938e8b3cfb9fd50c021260417bea8c20af2
Ibad Rashid <siobe14@hotmail.com>
2024-03-31 21:59:10 +0000

    filled out page with User config table and general markup

--- src/app/dashboard/account-management/page.tsx
+++ src/app/dashboard/account-management/page.tsx
@@ -1,3 +1,28 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ConfigureUsersTable } from "./configure-users-table/table";
+
 export default function AccountManagementPage() {
-  return <h1>Account Management</h1>;
+  return (
+    <section className="flex w-full flex-col items-center justify-start gap-4 px-2 pt-2 sm:items-start">
+      <h1 className="text-2xl font-bold underline">Account Management</h1>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle className="text-xl font-medium">Configure Users</CardTitle>
+        </CardHeader>
+        <CardContent className="w-fit max-w-full sm:min-w-[50%]">
+          <ConfigureUsersTable />
+        </CardContent>
+      </Card>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle className="text-xl font-medium">
+            Configure Categories
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="w-fit max-w-full sm:min-w-[50%]">
+          <div>Insert editable table here</div>
+        </CardContent>
+      </Card>
+    </section>
+  );
 }
