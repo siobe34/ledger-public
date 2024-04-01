a9c194fbea0fd3723ddd22b851915805e0277921
Ibad Rashid <siobe14@hotmail.com>
2024-04-01 17:28:53 +0000

    testing how to construct this feature

--- src/app/dashboard/transactions/upload/page.tsx
+++ src/app/dashboard/transactions/upload/page.tsx
@@ -1,143 +1,150 @@
-// "use client";
+"use client";
 
-import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  type CarouselApi,
+} from "@/components/ui/carousel";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useState } from "react";
 
-// import { insertTransactionSchema } from "@/server/db/schema";
-// import { api } from "@/trpc/react";
-// import { createRef } from "react";
-// import { z } from "zod";
+const COLUMNS = [
+  { label: "Date", order: 1 },
+  { label: "Description", order: 2 },
+  { label: "Debit", order: 3 },
+  { label: "Credit", order: 4 },
+  { label: "Balance", order: 5 },
+  { label: "Category", order: 6 },
+  { label: "User", order: 7 },
+  { label: "Account", order: 8 },
+  { label: "Comments", order: 9 },
+];
 
 export default function UploadTransactionsPage() {
-  // const inputRef = createRef<HTMLInputElement>();
+  const [api, setApi] = useState<CarouselApi>();
+  const [canGoNext, setCanGoNext] = useState(true);
+  const [canGoPrev, setCanGoPrev] = useState(false);
 
-  // const test = api.transactions.create.useMutation();
-  // const loadCSVData = async () => {
-  //   const file = inputRef.current?.files;
+  const handleNext = () => {
+    api?.scrollNext();
 
-  //   // If no file exists or not a CSV file
-  //   if (!file || !file[0] || file[0].type !== "text/csv") {
-  //     return [];
-  //   }
+    if (!api?.canScrollNext()) setCanGoNext(false);
+    if (api?.canScrollPrev()) setCanGoPrev(true);
+  };
 
-  //   const CSVPromise = await fetch(URL.createObjectURL(file[0]));
-  //   const CSVFile = await CSVPromise.text();
+  const handlePrevious = () => {
+    api?.scrollPrev();
 
-  //   const rawData = CSVtoJSON(CSVFile, [
-  //     // "sequence",
-  //     // "transactionDate",
-  //     // "description",
-  //     // "debit",
-  //     // "credit",
-  //     // "balance",
-  //     // "user",
-  //     // "account",
-  //     // "category",
-  //     // "comments",
-  //     "sequence",
-  //     "account",
-  //     "balance",
-  //     "credit",
-  //     "transactionDate",
-  //     "debit",
-  //     "description",
-  //     "user",
-  //     "category",
-  //     "comments",
-  //   ]);
+    if (api?.canScrollNext()) setCanGoNext(true);
+    if (!api?.canScrollPrev()) setCanGoPrev(false);
+  };
 
-  //   const sanitizedData = rawData.map((rawTransaction) => {
-  //     const transaction: { [key: string]: string | number | Date } = {
-  //       ...rawTransaction,
-  //     };
-  //     if (rawTransaction["sequence"]) {
-  //       transaction["sequence"] = +rawTransaction.sequence || "";
-  //     }
-  //     if (rawTransaction["transactionDate"]) {
-  //       transaction["transactionDate"] = new Date(
-  //         rawTransaction.transactionDate,
-  //       );
-  //     }
-  //     if (rawTransaction["debit"] || rawTransaction["debit"] === "") {
-  //       transaction["debit"] =
-  //         rawTransaction.debit === ""
-  //           ? "0.00"
-  //           : parseFloat(rawTransaction.debit).toFixed(2).toString();
-  //     }
-  //     if (rawTransaction["credit"] || rawTransaction["credit"] === "") {
-  //       transaction["credit"] =
-  //         rawTransaction.credit === ""
-  //           ? "0.00"
-  //           : parseFloat(rawTransaction.credit).toFixed(2).toString();
-  //     }
-  //     if (rawTransaction["balance"] || rawTransaction["balance"] === "") {
-  //       transaction["balance"] =
-  //         rawTransaction.balance === ""
-  //           ? "0.00"
-  //           : parseFloat(rawTransaction.balance).toFixed(2).toString();
-  //     }
-
-  //     return { ...transaction };
-  //   });
-
-  //   return sanitizedData;
-  // };
-
-  // const uploadTransactions = async () => {
-  //   const csvData = await loadCSVData();
-  //   const zodParse = z.array(insertTransactionSchema).safeParse(csvData);
-
-  //   if (zodParse.success) {
-  //     const parsedData = zodParse.data;
-  //     console.log("zod success", parsedData);
-  //     test.mutate(parsedData);
-  //   } else {
-  //     console.log("zod error", zodParse.error.errors);
-  //   }
-  // };
   return (
     <>
-      <Link
-        href="/"
-        className="rounded border border-blue-500 bg-blue-500 px-4 py-2 text-white hover:bg-blue-800 hover:text-slate-300"
-      >
-        Home
-      </Link>
-      {/* <input
-        ref={inputRef}
-        type="file"
-        onChange={(event) => {
-          uploadTransactions();
-          // Set input value to empty string so user can select the same file again while still triggering onChange event
-          event.target.value = "";
-        }}
-        className="hidden"
-      />
-
-      <button
-        className="bg-blue-500 px-4 py-2 text-white hover:opacity-85"
-        onClick={() => inputRef.current?.click()}
-      >
-        Upload Data
-      </button> */}
+      <h1 className="text-2xl font-bold underline">Upload Transactions Data</h1>
+      <div className="flex w-full flex-wrap items-center justify-center gap-4">
+        <Button
+          variant="outline"
+          onClick={handlePrevious}
+          disabled={!canGoPrev}
+        >
+          Previous Step
+          <span className="sr-only" aria-disabled={!canGoPrev}>
+            Go to previous step in the Uploading Data wizard.
+          </span>
+        </Button>
+        <span className="hidden h-1 flex-grow border bg-border sm:flex" />
+        <Button variant="outline" onClick={handleNext} disabled={!canGoNext}>
+          Next Step
+          <span className="sr-only" aria-disabled={!canGoNext}>
+            Go to next step in the Uploading Data wizard.
+          </span>
+        </Button>
+      </div>
+      <Carousel setApi={setApi} className="self-center">
+        <CarouselContent>
+          <CarouselItem>
+            <Card className="mx-auto w-4/5 max-w-[80%]">
+              <CardHeader>
+                <CardTitle>Define your data</CardTitle>
+                <CardDescription>
+                  Transactions data can be uploaded in CSV format. The required
+                  columns for the data are shown below. If your data does not
+                  have the required fields then you can still upload what you
+                  have and then fill out the missing columns. Making sure to set
+                  the order of the columns correctly.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead>Column Name</TableHead>
+                      <TableHead>Column Order</TableHead>
+                      <TableHead>Column Included</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {COLUMNS.map((col) => (
+                      <TableRow key={col.order}>
+                        <TableCell>{col.label}</TableCell>
+                        <TableCell>
+                          <Input
+                            type="text"
+                            defaultValue={col.order}
+                            className="w-12 max-w-full text-center"
+                          />
+                        </TableCell>
+                        <TableCell>
+                          <input type="checkbox" defaultChecked />
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </CardContent>
+            </Card>
+          </CarouselItem>
+          <CarouselItem>
+            <Card className="mx-auto w-4/5 max-w-[80%]">
+              <CardHeader>
+                <CardTitle>Step 2</CardTitle>
+                <CardDescription>Just a test</CardDescription>
+              </CardHeader>
+              <CardContent>Some other things go here</CardContent>
+            </Card>
+          </CarouselItem>
+          <CarouselItem>
+            <Card className="mx-auto w-4/5 max-w-[80%]">
+              <CardHeader>
+                <CardTitle>Step 3</CardTitle>
+                <CardDescription>
+                  Testing some more and writing a bit more too, just another
+                  half sentence.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>Much shorter</CardContent>
+            </Card>
+          </CarouselItem>
+        </CarouselContent>
+      </Carousel>
     </>
   );
 }
-
-// const CSVtoJSON = (CSV: string, headers: string[]) => {
-//   const rows = CSV.split("\n").filter((row) => row !== "");
-
-//   const json = rows.map((row) => {
-//     // * Split the current row into an array of values and remove any potential whitespaces
-//     const values = row.split(",").map((val) => val.replace(/\s+/g, " ").trim());
-
-//     const rowObject: { [key: string]: string } = {};
-
-//     headers.forEach((key, index) => {
-//       rowObject[key] = values[index] || "";
-//     });
-
-//     return rowObject;
-//   });
-
-//   return json;
-// };
