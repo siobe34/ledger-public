8ab7bb436b30d68cf69ab51680032dfecb836a9f
Ibad Rashid <siobe14@hotmail.com>
2024-04-14 22:54:17 +0000

    added state for dropdowns to zustand

--- src/lib/store/upload-transactions-wizard/step2slice.ts
+++ src/lib/store/upload-transactions-wizard/step2slice.ts
@@ -1,11 +1,19 @@
 import { type StateCreator } from "zustand";
 
 export type Step2Slice = {
+  user: string;
+  setUser: (user: string) => void;
+  account: string;
+  setAccount: (account: string) => void;
   uploadedData: Record<string, string>[] | null;
   setUploadedData: (newData: Record<string, string>[]) => void;
 };
 
 export const createStep2Slice: StateCreator<Step2Slice> = (set) => ({
+  user: "User",
+  setUser: (user) => set({ user }),
+  account: "Account",
+  setAccount: (account) => set({ account }),
   uploadedData: null,
   setUploadedData: (newData) => set({ uploadedData: newData }),
 });
