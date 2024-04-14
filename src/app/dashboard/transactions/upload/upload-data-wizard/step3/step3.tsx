ca09db63c509a1fd5578ad1842843ae759897036
Ibad Rashid <siobe14@hotmail.com>
2024-04-14 15:52:06 +0000

    get configured categories to pass to editable Table

--- src/app/dashboard/transactions/upload/upload-data-wizard/step3/step3.tsx
+++ src/app/dashboard/transactions/upload/upload-data-wizard/step3/step3.tsx
@@ -8,8 +8,12 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { api } from "@/trpc/react";
 
 export const Step3 = () => {
+  // TODO: this entire page can be RSC and this data fetching can take place on server
+  const { data: categories } = api.relatedCategories.get.useQuery();
+
   return (
     <Card>
       <CardHeader>
