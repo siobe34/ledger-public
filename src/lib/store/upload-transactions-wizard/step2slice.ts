e8f2d83118eb6e58d802ca548a87e66ba1e4c7c6
Ibad Rashid <siobe14@hotmail.com>
2024-04-14 04:24:30 +0000

    state management for step 2 of upload transactions wizard

--- src/lib/store/upload-transactions-wizard/step2slice.ts
+++ src/lib/store/upload-transactions-wizard/step2slice.ts
@@ -1,7 +1,11 @@
 import { type StateCreator } from "zustand";
 
 export type Step2Slice = {
-  //
+  uploadedData: Record<string, string>[] | null;
+  setUploadedData: (newData: Record<string, string>[]) => void;
 };
 
-export const createStep2Slice: StateCreator<Step2Slice> = (set) => ({});
+export const createStep2Slice: StateCreator<Step2Slice> = (set) => ({
+  uploadedData: null,
+  setUploadedData: (newData) => set({ uploadedData: newData }),
+});
--- src/lib/store/upload-transactions-wizard/store.ts
+++ src/lib/store/upload-transactions-wizard/store.ts
@@ -3,14 +3,19 @@ import {
   type Step1Slice,
 } from "@/lib/store/upload-transactions-wizard/step1slice";
 import {
+  createStep2Slice,
+  type Step2Slice,
+} from "@/lib/store/upload-transactions-wizard/step2slice";
+import {
   createWizardStepManagerSlice,
   type WizardStepsManager,
 } from "@/lib/store/upload-transactions-wizard/wizard-step-manager";
 import { create } from "zustand";
 
 export const useUploadTransactionsWizard = create<
-  WizardStepsManager & Step1Slice
+  WizardStepsManager & Step1Slice & Step2Slice
 >()((...a) => ({
   ...createWizardStepManagerSlice(...a),
   ...createStep1Slice(...a),
+  ...createStep2Slice(...a),
 }));
