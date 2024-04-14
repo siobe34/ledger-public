cb4aaeced8da3518ac77b059709c8250847ca8dd
Ibad Rashid <siobe14@hotmail.com>
2024-04-14 02:07:14 +0000

    upload transactions step 3

---
+++ src/app/dashboard/transactions/upload/upload-data-wizard/step3/step3.tsx
@@ -0,0 +1,19 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export const Step3 = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Step 3</CardTitle>
+        <CardDescription>Just a test</CardDescription>
+      </CardHeader>
+      <CardContent>Some other things go here</CardContent>
+    </Card>
+  );
+};
