95258b18e152b9b5b014ff51b9dee4d660883f31
Ibad Rashid <siobe14@hotmail.com>
2024-04-14 22:57:06 +0000

    url params for wizard state management

--- src/app/dashboard/transactions/upload/page.tsx
+++ src/app/dashboard/transactions/upload/page.tsx
@@ -1,14 +1,31 @@
 import { UploadDataWizard } from "@/app/dashboard/transactions/upload/upload-data-wizard/wizard";
 import { api } from "@/trpc/server";
 import { redirect } from "next/navigation";
+import { z } from "zod";
 
-export default async function UploadTransactionsPage() {
+// TODO: fix url param parsing and zod schema
+const test = z.object({ step: z.number() });
+
+export default async function UploadTransactionsPage({
+  searchParams,
+}: {
+  searchParams: Record<string, string | string[] | undefined>;
+}) {
   const configuredUsers = await api.relatedUsers.get.query();
   const configuredCategories = await api.relatedCategories.get.query();
+  const unsafeParams = { step: searchParams.step && +searchParams.step };
 
   if (configuredUsers.length === 0 || configuredCategories.length === 0) {
     redirect("/dashboard/transactions/upload/config-required");
   }
+
+  const zodStatus = test.safeParse(unsafeParams);
+
+  if (!zodStatus.success) {
+    redirect("/dashboard/transactions/upload?step=1");
+  }
+
+  const parsedSearchParams = zodStatus.data;
 
   return (
     <>
