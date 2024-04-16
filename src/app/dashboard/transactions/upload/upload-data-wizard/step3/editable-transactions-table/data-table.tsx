c480a67bb32f342dde32a94834b8da29a64e0380
Ibad Rashid <siobe14@hotmail.com>
2024-04-16 06:19:01 +0000

    editable data table for uploading transactions

---
+++ src/app/dashboard/transactions/upload/upload-data-wizard/step3/editable-transactions-table/data-table.tsx
@@ -0,0 +1,173 @@
+"use client";
+
+import { Pagination } from "@/components/data-table/pagination";
+import { TableSearch } from "@/components/data-table/table-search";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useUploadTransactionsWizard } from "@/lib/store/upload-transactions-wizard/store";
+import {
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+  type ColumnDef,
+  type RowData,
+  type SortingState,
+} from "@tanstack/react-table";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+declare module "@tanstack/react-table" {
+  interface TableMeta<TData extends RowData> {
+    updateData: (rowIndex: number, columnId: string, value: unknown) => void;
+    categories: string[];
+    users: string[];
+  }
+}
+
+type DataTableProps<TData, TValue> = {
+  categories: string[];
+  users: string[];
+  columns: ColumnDef<TData, TValue>[];
+  initialData: TData[];
+  searchBar?: boolean;
+  pagination?: boolean;
+};
+
+function useSkipper() {
+  const shouldSkipRef = useRef(true);
+  const shouldSkip = shouldSkipRef.current;
+
+  const skip = useCallback(() => {
+    shouldSkipRef.current = false;
+  }, []);
+
+  useEffect(() => {
+    shouldSkipRef.current = true;
+  });
+
+  return [shouldSkip, skip] as const;
+}
+
+export const EditableTransactionsTable = <TData, TValue>({
+  categories,
+  users,
+  columns,
+  initialData,
+  searchBar = true,
+  pagination = true,
+}: DataTableProps<TData, TValue>) => {
+  const { setTableData } = useUploadTransactionsWizard();
+
+  const [data, setData] = useState(initialData);
+
+  const [globalFilter, setGlobalFilter] = useState("");
+  const [sorting, setSorting] = useState<SortingState>([]);
+
+  const [autoResetPageIndex, skipAutoResetPageIndex] = useSkipper();
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    onGlobalFilterChange: setGlobalFilter,
+    onSortingChange: setSorting,
+    autoResetPageIndex,
+    state: {
+      globalFilter,
+      sorting,
+    },
+    meta: {
+      categories,
+      users,
+      updateData: (rowIndex, columnId, value) => {
+        // Skip page index reset until after next rerender
+        skipAutoResetPageIndex();
+        setData((old) =>
+          old.map((row, index) => {
+            if (index === rowIndex) {
+              return {
+                ...old[rowIndex]!,
+                [columnId]: value,
+              };
+            }
+            return row;
+          }),
+        );
+      },
+    },
+  });
+
+  useEffect(() => {
+    setTableData(data);
+  }, [data, setTableData]);
+
+  return (
+    <>
+      {searchBar && (
+        <TableSearch search={globalFilter} setSearch={setGlobalFilter} />
+      )}
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+        {pagination && <Pagination table={table} />}
+      </div>
+    </>
+  );
+};
