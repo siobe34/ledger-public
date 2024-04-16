e21cbcd4d85b5229298e63071b3d2a33b8c40c63
Ibad Rashid <siobe14@hotmail.com>
2024-04-16 06:03:29 +0000

    removing unnecessary separator

--- src/components/data-table/data-table-column-header.tsx
+++ src/components/data-table/data-table-column-header.tsx
@@ -3,7 +3,6 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utilsCn";
@@ -57,7 +56,6 @@ export const DataTableColumnHeader = <TData, TValue>({
             <ArrowDownIcon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
             Desc
           </DropdownMenuItem>
-          <DropdownMenuSeparator />
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
