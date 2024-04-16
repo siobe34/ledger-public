3129728c3706a634a068679427bd1a9c87fad69a
Ibad Rashid <siobe14@hotmail.com>
2024-04-16 08:31:17 +0000

    proceed to step 3 when data is uploaded

--- src/app/dashboard/transactions/upload/upload-data-wizard/step2/step2-actions.tsx
+++ src/app/dashboard/transactions/upload/upload-data-wizard/step2/step2-actions.tsx
@@ -1,15 +1,34 @@
-import { buttonVariants } from "@/components/ui/button";
-import Link from "next/link";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUploadTransactionsWizard } from "@/lib/store/upload-transactions-wizard/store";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 export const Step2Actions = () => {
+  const router = useRouter();
+  const [validationStatus, setValidationStatus] = useState(false);
+  const { uploadedData } = useUploadTransactionsWizard();
+
+  useEffect(() => {
+    if (!uploadedData) return;
+
+    // TODO: additional validation required here to make sure uploadedData is in a shape readable by the upload transactions table in step 3, otherwise it throws an Error
+
+    toast.success("Ready to proceed to Step 3.");
+
+    setValidationStatus(true);
+  }, [uploadedData]);
+
   return (
     <div className="flex flex-wrap items-center justify-center gap-8 py-4">
-      <Link
-        href="/dashboard/transactions/upload?step=3"
-        className={buttonVariants({ variant: "default" })}
+      <Button
+        onClick={() => router.push("/dashboard/transactions/upload?step=3")}
+        disabled={!validationStatus}
       >
         Step 3
-      </Link>
+      </Button>
     </div>
   );
 };
