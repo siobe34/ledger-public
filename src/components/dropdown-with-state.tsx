e5c4ee973ff25ba7b8ca2de683dd0f7e94e68ec9
Ibad Rashid <siobe14@hotmail.com>
2024-04-16 06:02:12 +0000

    stateful dropdown component

---
+++ src/components/dropdown-with-state.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { Button, type ButtonProps } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  type DropdownMenuContentProps,
+} from "@/components/ui/dropdown-menu";
+
+export type DropdownStatefulProps = {
+  buttonProps?: ButtonProps;
+  dropdownOpts: string[];
+  selectedItem: DropdownStatefulProps["dropdownOpts"][number];
+  setItemCallback?: (item: DropdownStatefulProps["selectedItem"]) => void;
+  setSelectedItem:
+    | React.Dispatch<
+        React.SetStateAction<DropdownStatefulProps["selectedItem"]>
+      >
+    | ((item: DropdownStatefulProps["selectedItem"]) => void);
+} & DropdownMenuContentProps;
+
+export const DropdownStateful = ({
+  align = "center",
+  buttonProps,
+  dropdownOpts,
+  selectedItem,
+  setItemCallback,
+  setSelectedItem,
+  ...props
+}: DropdownStatefulProps) => {
+  const handleSelect = (newItem: DropdownStatefulProps["selectedItem"]) => {
+    setSelectedItem(newItem);
+
+    if (setItemCallback) {
+      setItemCallback(newItem);
+    }
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button {...buttonProps}>{selectedItem}</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent {...props} align={align} loop>
+        {dropdownOpts.map((dropdownItem) => (
+          <DropdownMenuItem
+            key={dropdownItem}
+            className="cursor-pointer justify-center"
+            onSelect={() => handleSelect(dropdownItem)}
+          >
+            {dropdownItem}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
