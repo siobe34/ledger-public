a165638d9c3ec3986a964289c1734ea5e8ea02f5
Ibad Rashid <siobe14@hotmail.com>
2024-04-17 01:52:27 +0000

    get type from state

--- src/lib/store/upload-transactions-wizard/step3slice.ts
+++ src/lib/store/upload-transactions-wizard/step3slice.ts
@@ -2,7 +2,7 @@ import { type StateCreator } from "zustand";
 
 export type Step3Slice = {
   tableData: unknown[];
-  setTableData: (newData: unknown[]) => void;
+  setTableData: (newData: Step3Slice["tableData"]) => void;
 };
 
 export const createStep3Slice: StateCreator<Step3Slice> = (set) => ({
