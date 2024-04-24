315b16687920a934eb1214b6a84255c627b21724
Ibad Rashid <siobe14@hotmail.com>
2024-04-24 16:44:41 +0000

    redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <section>
-      <button className="rounded bg-secondary p-4 text-secondary-foreground">
-        Test
-      </button>
-      <a
-        href="/dashboard"
-        className="rounded border border-blue-500 bg-blue-500 px-4 py-2 text-white hover:bg-blue-800 hover:text-slate-300"
-      >
-        Dashboard
-      </a>
-    </section>
-  );
+  redirect("/auth/login");
 }
